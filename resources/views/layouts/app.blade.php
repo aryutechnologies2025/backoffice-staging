b2a9e8365f4c24d607cdd47537e6fec8327d5bc7
BarathKrishnamoorthy <barathkrishnamoorthy17@gmail.com>
2024-12-26 17:20:44 +0530

    check

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
     <link href="https://cdn.jsdelivr.net/npm/summernote/dist/summernote-lite.min.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
   <!-- Include DataTables CSS and JS -->
-
+<!--testing-->
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 
