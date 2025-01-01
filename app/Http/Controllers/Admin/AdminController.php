2db491f61b9b789f5427afa7e68f35dd076fbe54
BarathKrishnamoorthy <barathkrishnamoorthy17@gmail.com>
2025-01-01 20:59:25 +0530

    removing the delay in login

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -63,30 +63,7 @@ class AdminController extends Controller
         ]);
         $email = $request->input('email');
         $password = $request->input('password');
-        $admin = null;
-        $retryCount = 0;
-        $maxRetries = 5;
-        $retryDelay = 1000; // in milliseconds
-
-        while ($retryCount < $maxRetries) {
-            try {
-                $admin = Admin::where('email', $email)->first();
-                break; // Exit loop if successful
-            } catch (\Illuminate\Database\QueryException $ex) {
-                if ($ex->getCode() == 1226) { // MySQL error code for max_connections_per_hour exceeded
-                    $retryCount++;
-                    usleep($retryDelay * 1000); // Convert milliseconds to microseconds
-                } else {
-                    throw $ex; // Rethrow if it's a different error
-                }
-            }
-        }
-
-        if (!$admin) {
-            return back()->withErrors([
-                'email' => 'Unable to process your request at the moment. Please try again later.',
-            ])->onlyInput('email');
-        }
+        $admin = Admin::where('email', $email)->first();
 
         if (Auth::guard('admin')->attempt(['email' => $email, 'password' => $password])) {
             $request->session()->regenerate();
