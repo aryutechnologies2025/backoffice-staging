5caff8f42c0e3ed37f2c06df4191b2364fd023b4
BarathKrishnamoorthy <barathkrishnamoorthy17@gmail.com>
2025-01-24 12:51:05 +0530

    Update reviewlist.blade.php

--- resources/views/admin/review/reviewlist.blade.php
+++ resources/views/admin/review/reviewlist.blade.php
@@ -57,7 +57,7 @@
                     <tr>
                     <td class="text-center">{{ $loop->iteration }}</td>
 
-                    <td class="text-center"><img src="{{ asset($row->user->profile_image ?? '/assets/image/login/inner_pece_logo.png') }}" alt="Thumbnail" style="max-width: 100px; max-height: 100px; object-fit: cover;"></td>
+                    <td class="text-center"><img src="{{ asset($row->user->profile_image ?? '/assets/image/dashboard/innerpece_toogle_icon.svg') }}" alt="Thumbnail" style="max-width: 100px; max-height: 100px; object-fit: cover;"></td>
                         <td class="text-center">{{ $row->user->first_name }}</td>
                         <td class="text-center">{{ $row->package->title }}</td>
                         <td class="text-center">{{ $row->rating }}</td>
