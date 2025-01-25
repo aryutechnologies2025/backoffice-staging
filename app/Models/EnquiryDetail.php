2c1511e31e64f48807ee9a4dabc9a3aefeca9c75
BarathKrishnamoorthy <barathkrishnamoorthy17@gmail.com>
2025-01-25 15:31:25 +0530

    Update EnquiryDetail.php

--- app/Models/EnquiryDetail.php
+++ app/Models/EnquiryDetail.php
@@ -29,7 +29,7 @@ class EnquiryDetail extends Model
         'program_title',
         'child_count',
         'child_age',
-        'user_id',
+        
         'users_id'
     ];
     public function package()
