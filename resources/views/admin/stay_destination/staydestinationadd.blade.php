2bd10c196c6828fccfb256dd325e6dc1f4ec3ed6
Heyram33 <heyram334@gmail.com>
2025-05-28 17:52:09 +0530

    updated stay design

--- resources/views/admin/stay_destination/staydestinationadd.blade.php
+++ resources/views/admin/stay_destination/staydestinationadd.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 @Section('content')
+<style>
+    .form-check-input {
+        transform: scale(1.5);
+     
+        
+    }
+   
+</style>
 <div class="row body-sec py-5  px-5 justify-content-around">
     <div class="col-lg-12">
         <h3 class="fw-bold"><span class="vr"></span>&nbsp;{{$title}}</h3>
@@ -8,15 +16,15 @@
     <!-- FORM -->
     <div class="row mb-5">
         <div class="col">
-            <div class="form-body px-5 py-5 rounded-4 m-auto ">
+            <div class="form-bodyc py-5 rounded-4 m-auto ">
                 <form id="form_valid" action="{{ route('admin.staydestination_insert') }}" method="POST" autocomplete="off" enctype="multipart/form-data">
                     @csrf
                     <div class="mb-3">
-                    <div class="g-2 mb-4">
-                            <div class="col">
+                        <div class="g-2 mb-4 d-flex gap-5 align-items-center">
+                            <div class="col-2">
                                 <label class="fw-bold mt-4"> Image </label>
                                 <div class="row d-flex mb-4">
-                                    <div class="col-lg-2">
+                                    <div class="w-100 mt-2">
                                         <div class="form-input">
                                             <label for="file-ip-1" class="px-4 py-3 text-center">
                                                 <img class="text-center mt-3" id="file-ip-1-preview" src="/assets/image/dashboard/innerpece_addpic_icon.svg">
@@ -30,13 +38,29 @@
 
 
                                 </div>
+
                                 <!-- <h6>*Supported formats: PNG & JPG; File size limit: 2 MB</h6> -->
+                            </div>
+                            <div class="col-4 ms-5">
+                                <div class="g-2 mb-4">
+                                    <div class="col">
+                                        <label class="fw-bold mb-2">Upload Image Name <span class="text-danger">*</span></label>
+                                        <input type="text" placeholder="Rename the Photo" id="upload_image_name" name="upload_image_name" value="{{ old('upload_image_name') }}" class="form-control py-3 rounded-3 shadow-sm" required>
+                                    </div>
+                                </div>
+
+                                <div class=" g-2 ">
+                                    <div class="col">
+                                        <label class="fw-bold mb-2">Alternate Image Name <span class="text-danger">*</span></label>
+                                        <input type="text" placeholder="Alternate Name" id="alternate_image_name" name="alternate_image_name" value="{{ old('alternate_image_name') }}" class="form-control py-3 rounded-3 shadow-sm" required>
+                                    </div>
+                                </div>
                             </div>
                         </div>
 
-                        <div class="row g-2 mb-4">
+                        <div class="col-4 g-2 mb-4">
                             <div class="col">
-                                <label class="fw-bold mb-4 "> Destination <span class="text-danger">*</span></label>
+                                <label class="fw-bold mb-2 "> Destination <span class="text-danger">*</span></label>
                                 <input type="text" placeholder="Destination" id="destination_name" name="destination_name" class="form-control py-3 rounded-3 shadow-sm" required value="{{old('destination_name')}}">
                             </div>
                         </div>
@@ -46,24 +70,12 @@
                                 <input type="text" placeholder="Place" id="place" name="place" class="form-control py-3 rounded-3 shadow-sm" required value="{{old('place')}}">
                             </div>
                         </div> -->
-             
-                        <div class="row g-2 mb-4">
-                            <div class="col">
-                                <label class="fw-bold mb-4">Upload Image Name <span class="text-danger">*</span></label>
-                                <input type="text" placeholder="Rename the Photo" id="upload_image_name" name="upload_image_name" value="{{ old('upload_image_name') }}" class="form-control py-3 rounded-3 shadow-sm" required>
-                            </div>
-                        </div>
 
-                        <div class="row g-2 mb-4">
-                            <div class="col">
-                                <label class="fw-bold mb-4">Alternate Image Name <span class="text-danger">*</span></label>
-                                <input type="text" placeholder="Alternate Name" id="alternate_image_name" name="alternate_image_name" value="{{ old('alternate_image_name') }}" class="form-control py-3 rounded-3 shadow-sm" required>
-                            </div>
-                        </div>
+
                         <div class="row g-2">
                             <div class="col">
-                                <label class="fw-bold ">Status</label>
-                                <div class="form-check form-switch">
+                                <label class="fw-bold mb-2">Status</label>
+                                <div class="form-check form-switch ms-2">
                                     <input class="form-check-input check_bx" type="checkbox" id="status" name="status">
                                 </div>
                             </div>
--- resources/views/admin/stay_destination/staydestinationedit.blade.php
+++ resources/views/admin/stay_destination/staydestinationedit.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 @section('content')
+<style>
+    .form-check-input {
+        transform: scale(1.5);       
+    }
+   
+</style>
 
 <div class="row body-sec py-5 px-5 justify-content-around">
     <div class="col-lg-12">
@@ -8,60 +14,63 @@
 
     <div class="row mb-5">
         <div class="col">
-            <div class="form-body px-5 py-5 rounded-4 m-auto ">
+            <div class="form-body py-5 rounded-4  ">
                 <form id="form_valid" action="{{ route('admin.staydestination_update', ['id' => $destination_details->id]) }}" method="POST" autocomplete="off" enctype="multipart/form-data">
                     @csrf
-                    <div class="mb-3">
-                        <div class="row g-2 mb-4">
-                            <div class="col">
-                                <label class="fw-bold mb-4"> Destination <span class="text-danger">*</span></label>
-                                <input type="text" placeholder="Destination" id="city_name" name="city_name" value="{{ $destination_details->city_name }}" class="form-control py-3 rounded-3 shadow-sm" required>
+                        <div class="g-2 mb-4 ">
+                    <div class="g-2 mb-4 d-flex gap-5 align-items-center">
+                        <div class="col-3">
+                            <label class="fw-bold mt-4 mb-2"> Image </label>
+                            <div class="row d-flex mb-4">
+                                <div class="w-100">
+                                    <div class="form-input">
+                                        <!-- Existing image preview -->
+                                        @if ($destination_details->city_image)
+                                        <img id="file-ip-1-preview" src="{{ asset($destination_details->city_image) }}" alt="Thumbnail Preview" class="img-thumbnail mb-3" style="max-height: 150px; object-fit: cover;">
+                                        @else
+                                        <img id="file-ip-1-preview" src="/assets/image/dashboard/innerpece_addpic_icon.svg" alt="Add Pic" class="img-thumbnail mb-3">
+                                        @endif
+
+                                        <label for="file-ip-1" class="d-block text-center py-3" style="cursor: pointer;">
+                                            <p class="text-center fw-light">Update Pic</p>
+                                        </label>
+                                        <input type="file" id="file-ip-1" name="image_1" accept="image/png, image/jpeg, image/svg+xml" onchange="previewImage(event)">
+                                        <div id="file-ip-1-error" class="text-danger"></div>
+                                    </div>
+                                </div>
                             </div>
+                            <!-- <h6>*Supported formats: PNG & JPG; File size limit: 2 MB</h6> -->
                         </div>
-                        
-                        <div class="row g-2 mb-4">
+                        <div class="col-4 g-2">
                             <div class="col">
-                                <label class="fw-bold mb-4">Upload Image Name <span class="text-danger">*</span></label>
-                                <input type="text" placeholder="Rename the Photo" id="upload_image_name" name="upload_image_name" value="{{ $destination_details->upload_image_name }}" class="form-control py-3 rounded-3 shadow-sm" required>
+                                <label class="fw-bold mb-2">Upload Image Name <span class="text-danger">*</span></label>
+                                <input type="text" placeholder="Rename the Photo" id="upload_image_name" name="upload_image_name" value="{{ $destination_details->upload_image_name }}" class="form-control py-2 rounded-3 shadow-sm" required>
                             </div>
                         </div>
 
+                        <div class="col-4 g-2">
+                            <div class="col">
+                                <label class="fw-bold mb-2">Alternate Image Name <span class="text-danger">*</span></label>
+                                <input type="text" placeholder="Alternate Name" id="alternate_name" name="alternate_name" value="{{ $destination_details->alternate_name }}" class="form-control py-2 rounded-3 shadow-sm" required>
+                            </div>
+                        </div>
+
+
+                    </div>
+                    <div class="mb-3 col-4">
                         <div class="row g-2 mb-4">
                             <div class="col">
-                                <label class="fw-bold mb-4">Alternate Image Name <span class="text-danger">*</span></label>
-                                <input type="text" placeholder="Alternate Name" id="alternate_name" name="alternate_name" value="{{ $destination_details->alternate_name }}"  class="form-control py-3 rounded-3 shadow-sm" required>
+                                <label class="fw-bold mb-4"> Destination <span class="text-danger">*</span></label>
+                                <input type="text" placeholder="Destination" id="city_name" name="city_name" value="{{ $destination_details->city_name }}" class="form-control p-2 rounded-3 shadow-sm" required>
                             </div>
                         </div>
-                      
-                        <div class="g-2 mb-4">
-                            <div class="col">
-                                <label class="fw-bold mt-4"> Image </label>
-                                <div class="row d-flex mb-4">
-                                    <div class="col-lg-2">
-                                        <div class="form-input">
-                                            <!-- Existing image preview -->
-                                            @if ($destination_details->city_image)
-                                            <img id="file-ip-1-preview" src="{{ asset($destination_details->city_image) }}" alt="Thumbnail Preview" class="img-thumbnail mb-3" style="max-height: 150px; object-fit: cover;">
-                                            @else
-                                                <img id="file-ip-1-preview" src="/assets/image/dashboard/innerpece_addpic_icon.svg" alt="Add Pic" class="img-thumbnail mb-3">
-                                            @endif
-                                            
-                                            <label for="file-ip-1" class="d-block text-center py-3" style="cursor: pointer;">
-                                                <p class="text-center fw-light">Add Pic</p>
-                                            </label>
-                                            <input type="file" id="file-ip-1" name="image_1" accept="image/png, image/jpeg, image/svg+xml" onchange="previewImage(event)">
-                                            <div id="file-ip-1-error" class="text-danger"></div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- <h6>*Supported formats: PNG & JPG; File size limit: 2 MB</h6> -->
-                            </div>
-                        </div>
-                        
+
+
+
                         <div class="row g-2">
                             <div class="col">
                                 <label class="fw-bold">Status</label>
-                                <div class="form-check form-switch">
+                                <div class="form-check form-switch ms-2">
                                     <input class="form-check-input check_bx" type="checkbox" id="status" name="status" {{ $destination_details->status ? 'checked' : '' }}>
                                 </div>
                             </div>
@@ -81,15 +90,15 @@
 
 </div>
 <script>
-     function previewImage(event) {
+    function previewImage(event) {
         const preview = document.getElementById('file-ip-1-preview');
         const file = event.target.files[0];
         const reader = new FileReader();
-        
+
         reader.onload = function() {
             preview.src = reader.result;
         };
-        
+
         if (file) {
             reader.readAsDataURL(file);
         }
