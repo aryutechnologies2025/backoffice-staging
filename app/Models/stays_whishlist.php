504c4781220f9b75f3fa6754a698b5fad20c54a4
Heyram33 <heyram334@gmail.com>
2025-05-30 12:30:08 +0530

    updated get wishlist

--- app/Models/stays_whishlist.php
+++ app/Models/stays_whishlist.php
@@ -9,4 +9,14 @@ class stays_whishlist extends Model
 {
     use HasFactory;
    protected $fillable = ['user_id','stay_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function stay_dts()
+    {
+        return $this->belongsTo(stays_destination_details::class, 'stay_id');
+    }
 }
