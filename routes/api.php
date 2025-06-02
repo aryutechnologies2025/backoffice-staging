d50948e5f440960046b2836c362fd689800711e9
Heyram33 <heyram334@gmail.com>
2025-06-02 15:25:14 +0530

    updated stay enquiry api

--- routes/api.php
+++ routes/api.php
@@ -88,7 +88,7 @@ Route::prefix('v1')->group(function () {
         Route::get('/get-stay-details', [StayController::class, 'get_stay_details']);
 
         Route::get('/get-stay-destination', [StayDestinationController::class, 'get_stay_destination']);
-        Route::get('/store-stay-enquiry', [StayEnquiryController::class, 'store']);
+        Route::post('/store-stay-enquiry', [StayEnquiryController::class, 'store']);
     });
 });
 
