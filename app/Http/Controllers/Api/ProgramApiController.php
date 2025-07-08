28c38955c10e6eedbca37843547048e4b9005e96
BarathKrishnamoorthy <barathkrishnamoorthy17@gmail.com>
2025-07-08 10:55:24 +0530

    revert the perviois

--- app/Http/Controllers/Api/ProgramApiController.php
+++ app/Http/Controllers/Api/ProgramApiController.php
@@ -1722,8 +1722,7 @@ class ProgramApiController extends Controller
 
             $program = customer_package::find($programId);
             $Inclusivepackage = InclusivePackages::with('destination', 'theme', 'clientReviews', 'reviews')
-            ->where('is_deleted', 0)
-            ->where('status', 1)->find($program->package_id);
+            ->find($program->package_id);
 
             if (!$program) {
                 return response()->json([
