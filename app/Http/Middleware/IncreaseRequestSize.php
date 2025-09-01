f7a24004a6fb749c620df1ee153100ae87f44c04
Your Name <you@example.com>
2025-09-01 17:31:56 +0530

    updatelimit

--- app/Http/Middleware/IncreaseRequestSize.php
+++ app/Http/Middleware/IncreaseRequestSize.php
@@ -21,6 +21,8 @@ class IncreaseRequestSize
         ini_set('max_execution_time', 300);
         ini_set('max_input_time', 300);
 
+        ini_set('memory_limit', '512M');
+
         return $next($request);
     }
 }
