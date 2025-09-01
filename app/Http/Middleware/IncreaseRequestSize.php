61a2fa9e0750ffe096bcfad8b4d33f427259d188
Your Name <you@example.com>
2025-09-01 18:32:35 +0530

    updatelimit

--- app/Http/Middleware/IncreaseRequestSize.php
+++ app/Http/Middleware/IncreaseRequestSize.php
@@ -20,7 +20,7 @@ class IncreaseRequestSize
         ini_set('upload_max_filesize', '5120M');   // 5GB = 5120 MB
         ini_set('max_execution_time', '0');        // Unlimited execution time (or set to something like 3600 for 1h)
         ini_set('max_input_time', '-1');           // No limit on input parsing time
-        ini_set('memory_limit', '6G');             // Set higher than upload size (e.g. 6GB)
+        ini_set('memory_limit', '5120M');             // Set higher than upload size (e.g. 6GB)
 
         return $next($request);
     }
