4a1d0d1c9b72214a9ca324c01cc8a8acc0c109f8
Your Name <you@example.com>
2025-09-06 12:34:00 +0530

    updatesuccess

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -328,6 +328,7 @@ class AuthController extends Controller
             }
 
             return response()->json([
+                'success' => true,
                 'message' => 'Login successful!',
                 'token' => $token,
                 'user_details' => $userDetails,
